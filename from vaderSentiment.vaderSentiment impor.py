1b5f3fffe721e6e546876d8fbacdf3bc20893c73
TARUN N <75171612+Tarun-ece@users.noreply.github.com>
2023-07-12 22:23:38 +0530

    Update from vaderSentiment.vaderSentiment import.py

--- from vaderSentiment.vaderSentiment impor.py
+++ from vaderSentiment.vaderSentiment impor.py
@@ -1,7 +1,8 @@
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-Sentences=["you are looser but you play a good game"]
-analyzer=SentimentIntensityAnalyzer()
-for sentence in Sentences:
-    a=analyzer.polarity_scores(sentence)
-    
-    print("{:-<65} {}".format(sentence,str(a)))
+
+sentences = ["you are looser but you play a good game"]
+analyzer = SentimentIntensityAnalyzer()
+
+for sentence in sentences:
+    a = analyzer.polarity_scores(sentence)
+    print("{:-<65} {}".format(sentence, str(a)))
